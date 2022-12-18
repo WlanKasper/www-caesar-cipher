a2ee5bf3981594b53b9a43f8be591ea13dd87460
Artur Smirnov <artursmirnov@MBP-Artur.wind3.hub>
2022-12-18 22:35:35 +0100

    link routing

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import "./css/style.css";
 
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import Header from "./components/header/Header";
 import PageCipher from "./pages/PageCipher";
-// import PageBruteForce from "./pages/PageBruteForce";
+import PageBruteForce from "./pages/PageBruteForce";
+
+import ScrollToTop from "./utils/scrollToTop";
 
 const App = props => {
   return (
     <div className="App">
-      <Header /> 
-
-      <PageCipher />
-      {/* <PageBruteForce /> */}
+      <Router>
+        <ScrollToTop />
+        <Header />
+        <Routes>
+          <Route path="/" element={<PageCipher />} />
+          <Route path="/bruteforce" element={<PageBruteForce />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,22 +1,28 @@
 import './style.css';
 
+import { NavLink } from "react-router-dom";
+
 const Header = props => {
     return (<header>
         <h2 className="color-accent">
-            <a href="https://en.wikipedia.org/wiki/Caesar_cipher">
+            <NavLink to="/">
                 Caesar Cipher
-            </a>
+            </NavLink>
         </h2>
         <div className="wrapper-nav">
             <h4 className="color-main-extend">
-                <a href="http://localhost:3000/" className="underline">
+                <NavLink to="/" className={({ isActive }) => {
+                    return isActive ? 'underline' : '';
+                }}>
                     Cipher
-                </a>
+                </NavLink>
             </h4>
             <h4 className="color-main-extend">
-                <a href="http://localhost:3000/">
+                <NavLink to="/bruteforce" className={({ isActive }) => {
+                    return isActive ? 'underline' : '';
+                }}>
                     BruteForce
-                </a>
+                </NavLink>
             </h4>
         </div>
     </header>);
